a6329fe99aac4650473495309aead40c597619ef
yngtodd <young.todd.mk@gmail.com>
2019-02-12 15:43:34 -0500

    lstm up

--- scene/models/lstm.py
+++ scene/models/lstm.py
@@ -4,12 +4,15 @@ import torch.nn as nn
 
 class Hparams:
     def __init__(self, hidden_dim=50, emb_dim=300, 
-                 dropout=0.1, num_layers=1 num_linear=1):
+                 dropout=0.1, num_layers=2, num_linear=1):
         self.hidden_dim = hidden_dim 
         self.emb_dim = emb_dim
         self.dropout = dropout
         self.num_layers = num_layers
         self.num_linear = num_linear
+
+    def __repr__(self):
+        return str(Hparams().__dict__)
 
 
 class BiLSTM(nn.Module):
@@ -17,9 +20,9 @@ class BiLSTM(nn.Module):
     def __init__(self, num_vocab, n_classes, hparams=Hparams()):
         super().__init__()
         self.hparams = hparams
-        self.embedding = nn.Embedding(num_vocab), self.hparams.emb_dim)
-        self.encoder = _get_encoder() 
-        self.linear_layers = _get_linear_layers()
+        self.embedding = nn.Embedding(num_vocab, self.hparams.emb_dim)
+        self.encoder = self._get_encoder() 
+        self.linear_layers = self._get_linear_layers()
         self.predictor = nn.Linear(self.hparams.hidden_dim, n_classes)
 
     def _get_encoder(self):
@@ -31,11 +34,13 @@ class BiLSTM(nn.Module):
         )
         return encoder
 
-    def _get_linear_layers(self)
+    def _get_linear_layers(self):
         linear_layers = []
-        for _ in range(self.hparams.num_linear - 1):
-            linear_layers.append(nn.Linear(hidden_dim, hidden_dim))
-            linear_layers = nn.ModuleList(linear_layers)
+        for _ in range(self.hparams.num_linear-1):
+            linear_layers.append(
+                nn.Linear(self.hparams.hidden_dim, self.hparams.hidden_dim)
+            )
+        linear_layers = nn.ModuleList(linear_layers)
         return linear_layers
     
     def forward(self, seq):

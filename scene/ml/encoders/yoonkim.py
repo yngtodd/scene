a9a8b8313fc4ef41912ade051c6816c99e545f8a
yngtodd <young.todd.mk@gmail.com>
2019-02-22 01:08:15 -0500

    meh

--- scene/ml/encoders/yoonkim.py
+++ scene/ml/encoders/yoonkim.py
@@ -66,6 +66,7 @@ class YoonKimConv1DEncoder(Seq2VecEncoder):
         # convolution layers expect input of shape `(batch_size, in_channels, sequence_length)`,
         # where the conv layer `in_channels` is our `embedding_dim`.  We thus need to transpose the
         # tensor first.
+        print(f'tokens shape: {tokens.shape}')
         tokens = tokens.transpose(tokens, 1, 2)
         conv_features = []
         conv_features.append(self.block0(tokens))

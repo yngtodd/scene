ffeb4e883616fed31e9642d031f9a16af164d8f4
yngtodd <young.todd.mk@gmail.com>
2019-02-23 01:05:01 -0500

    new example

--- examples/conv_net/main.py
+++ examples/conv_net/main.py
@@ -7,15 +7,14 @@ from scene.data.reader import DataReader
 from scene.data.tokenizers import spacy_word_tokenizer
 
 from scene.ml.models import BaselineModel
-from allennlp.modules.seq2vec_encoders import CnnEncoder 
+from allennlp.modules.seq2vec_encoders import CnnEncoder
 
 from allennlp.training.trainer import Trainer
 from allennlp.data.vocabulary import Vocabulary
 from allennlp.data.iterators import BucketIterator
 
-from allennlp.modules.token_embedders import ElmoTokenEmbedder
+from allennlp.modules.token_embedders import Embedding
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
-from allennlp.data.token_indexers.elmo_indexer import ELMoTokenCharactersIndexer
 
 
 def main():
@@ -23,28 +22,28 @@ def main():
 
     torch.manual_seed(args.seed)
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda:2" if use_cuda else "cpu")
-
-    token_indexer = ELMoTokenCharactersIndexer()
+    device = torch.device("cuda:3" if use_cuda else "cpu")
 
     reader = DataReader(
         tokenizer=spacy_word_tokenizer,
-        token_indexers={"tokens": token_indexer}
     )
 
     iterator = BucketIterator(
         batch_size=args.batch_size,
-        sorting_keys=[("tokens", "num_tokens")],
+        sorting_keys=[("tokens", "num_tokens")]
     )
-
-    vocab = Vocabulary()
-    iterator.index_with(vocab)
 
     traindata = reader.read(args.datapath, 'train')
     valdata = reader.read(args.datapath, 'val')
+    testdata = reader.read(args.datapath, 'test')
 
-    elmo_embedder = ElmoTokenEmbedder(args.options_file, args.weight_file)
-    word_embeddings = BasicTextFieldEmbedder({"tokens": elmo_embedder})
+    vocab = Vocabulary.from_instances(traindata + valdata + testdata)
+    iterator.index_with(vocab)
+
+    token_embedding = Embedding(
+        num_embeddings=vocab.get_vocab_size('tokens'),
+        embedding_dim=args.embedding_dim
+    )
 
     encoder = CnnEncoder(
         embedding_dim=word_embeddings.get_output_dim(),
@@ -70,13 +69,17 @@ def main():
         train_dataset=traindata,
         validation_dataset=valdata,
         validation_iterator=iterator,
-        cuda_device=2,
-        patience=5,
+        cuda_device=3,
+        patience=15,
         num_epochs=args.num_epochs,
     )
 
     metrics = trainer.train()
-    print(metrics)
+
+    with open("./final_save/model.th", 'wb') as f:
+        torch.save(model.state_dict(), f)
+
+    vocab.save_to_files("./final_save/vocabulary")
 
 
 if __name__=='__main__':
